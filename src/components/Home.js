17e22799c04065f1925d4c408267bd0bd4901039
Elijah Soghomonian <2002elijahjs@gmail.com>
2024-08-10 22:29:13 -0700

    Created Hero Section +edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import "./Home.css"
 
 const Home = () => {
   return (
@@ -14,6 +15,17 @@ const Home = () => {
         <meta property="og:url" content="http://www.littlelemon.com" />
       </Helmet>
       {/* Page content starts undere here*/}
+      <div className='hero-container '>
+        <div className='left-box'>
+          <h1>Little Lemon</h1>
+          <h3>Chicago</h3>
+          <p>Indulge in the vibrant and aromatic flavors of Mediterranean cuisine right here in Chicago. From zesty lemon-infused dishes to savory classics, our menu celebrates the rich traditions and fresh ingredients of Mediterranean cooking. Join us for an unforgettable dining experience where every meal is a taste of the Mediterranean.</p>
+          <button>Reserve a Table</button>
+        </div>
+        <div className='right-box'>
+          <img src = "/assetsp/heroimg.jpg" alt="Hero image" className="hero-image" />
+        </div>
+      </div>
     </>
   );
 };
