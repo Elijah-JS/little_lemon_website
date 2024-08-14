21a00e2bc4a1c0af74a04a80cb3ef6687523f1b7
Elijah Soghomonian <2002elijahjs@gmail.com>
2024-08-14 01:55:44 -0700

    Created the entire application or website just need to link the buttons and add pop ups for certain buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./Login.css"
 import { Helmet } from 'react-helmet';
 
 const Login = () => {
@@ -14,6 +15,43 @@ const Login = () => {
         <meta property="og:url" content="http://www.littlelemon.com/login" />
       </Helmet>
       {/* Page content starts under here*/}
+
+
+      <div className='login-section'>
+      <div className='rows'>
+
+      <div className='leftside'> <img src='/assetsp/lemon.png' alt='lemon'/> </div>
+
+
+        <div className='middle'>
+        <p>One of us?</p>
+        <p>Log in to coninue</p>
+        <form className='login-form'>
+    <div className='loginbuttons'>
+        <div className='login-input-container' id='user'>
+            <img src='/assetsp/user.png' alt='username icon' className='input-icon'/>
+            <input type="text" name="username" placeholder="Username" required />
+        </div>
+        <div className='login-input-container'  id='pass'>
+            <img src='/assetsp/padlock.png' alt='password icon' className='input-icon'/>
+            <input type="password" name="password" placeholder="Password" required />
+        </div>
+        <button className='logbutton' type="submit">Login</button>
+    </div>
+</form>
+        </div>
+
+        <div className='rightside'> <img src='/assetsp/lemon.png' alt='lemon'/> </div>
+
+
+
+      </div>
+
+      </div>
+
+
+
+
     </>
   );
 };
